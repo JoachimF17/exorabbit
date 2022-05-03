267e47cac0cf903f566321b9e6130d72cbc230a2
Joachim <joachim.frances@hotmail.be>
2022-05-03 14:15:25 +0200

    Initial commit

---
+++ src/main/java/com/example/exorabbit/ExorabbitApplication.java
@@ -0,0 +1,13 @@
+package com.example.exorabbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExorabbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExorabbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/exorabbit/ExorabbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exorabbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExorabbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
